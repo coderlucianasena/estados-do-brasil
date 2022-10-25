353c193e9b0336093754ef9279ff6e02afe96817
Luciana Sena <38901230+englucianasena@users.noreply.github.com>
2022-10-25 18:48:25 -0300

    Correção

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require("express");
+const http = require("http");
+const estadoDatabase = require("./database/estados.json");
 
-const routes = require("./routes");
-
-const server = express();
-
-server.use(routes);
+const server = http.createServer ((req, resp) => {
+    resp.setHeader("Access-Control-Allow-Origin", "*");
+    resp.write (JSON.stringify(estadoDatabase));
+    resp.end();
+   
+});
 
 server.listen(8000);
